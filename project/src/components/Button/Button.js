696cc0f4b58d26f4df7ce457870701a28fd4f967
Priscila Flores <priscilauchoaa@gmail.com>
2022-08-02 18:33:34 +0200

    create reusable button

---
+++ project/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import './Button.css';
+function Button(props) {
+    return (
+        <button className={`btn-${props.variant}`} onClick={props.onClick}>
+            {props.text}
+        </button>
+    );
+}
+export default Button;
