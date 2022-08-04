4a6234b9ab1eed8061e50520e7ce5fc5fa395f1d
Priscila Flores <priscilauchoaa@gmail.com>
2022-08-04 21:28:56 +0200

    add page title component

---
+++ project/src/components/PageTitle/PageTitle.js
@@ -0,0 +1,11 @@
+import NavButton from "../NavButton/NavButton";
+import '../Button/Button.css'
+
+export default function PageTitle(props) {
+    return (
+        <div className='edit-btns'>
+            <h5>{props.title}</h5>
+            <NavButton link='/' variant='back' text='Back' />
+        </div>
+    );
+}
