1f6afb73e804aeacd43da274d42c77e3a198dc18
Priscila Flores <priscilauchoaa@gmail.com>
2022-08-06 00:08:28 +0200

    delete unncessary button

--- project/src/containers/components/Table/Table.js
+++ project/src/containers/components/Table/Table.js
@@ -4,12 +4,10 @@ export default function Table(props) {
     return (
         <tr className='fill'>
             <td key={props.id} className={props.left}>
-                {/* <button onClick={() => props.handleEdit(props.id)}> */}
                 <EditIcon
                     className='icn-edit'
                     onClick={() => props.handleEdit(props.id)}
                 />
-                {/* </button> */}
                 {props.token}
             </td>
             <td className={props.right}>{props.balance}</td>
