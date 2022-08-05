c97127e64129952418ce419cec61bced599b7027
Priscila Flores <priscilauchoaa@gmail.com>
2022-08-05 13:44:09 +0200

    create remove message component

--- project/src/components/RemoveMessage/RemoveMessage.js
+++ project/src/components/RemoveMessage/RemoveMessage.js
@@ -6,18 +6,20 @@ export default function RemoveMessage(props) {
         <div className='container'>
             <div>
                 <h4>Are you sure you want to delete your token?</h4>
-                <Button
-                    variant='save'
-                    onClick={props.handleRemoveConfirmation}
-                    text='YES'
-                />
-                <Button
-                    variant='remove'
-                    onClick={() => {
-                        props.setMessageRemove(false);
-                    }}
-                    text='NO'
-                />
+                <div className='btn-wrapper'>
+                    <Button
+                        variant='save'
+                        onClick={props.handleRemoveConfirmation}
+                        text='YES'
+                    />
+                    <Button
+                        variant='remove'
+                        onClick={() => {
+                            props.setMessageRemove(false);
+                        }}
+                        text='NO'
+                    />
+                </div>
             </div>
         </div>
     );
